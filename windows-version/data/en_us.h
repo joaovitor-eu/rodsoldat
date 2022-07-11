336c85e68045bc22d98ab76aeae88a12d8781093
João <91630610+joaovitor-eu@users.noreply.github.com>
2022-07-10 21:16:41 -0300

    Delete windows-version directory

--- windows-version/data/en_us.h
+++
@@ -1,9 +0,0 @@
-void static en_us()
-{
-    system("cls");
-    system("sudo cp -r C:/Users/%USERNAME%/Downloads F:/");
-    system("sudo cp -r C:/Users/%USERNAME%/Videos F:/");
-    system("sudo cp -r C:/Users/%USERNAME%/Desktop F:/");
-    system("sudo cp -r C:/Users/%USERNAME%/Images F:/");
-    system("clear");
-}
